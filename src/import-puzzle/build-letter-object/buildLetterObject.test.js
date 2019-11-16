e1a96ef46d95a80807fab6ae1bd3ea7005b2c4d0
terrill-chalmers <terrill-chalmers@protonmail.com>
2019-11-16 17:30:19 -0600

    chore: added integration test to create letterObject from textfile

--- src/import-puzzle/build-letter-object/buildLetterObject.test.js
+++ src/import-puzzle/build-letter-object/buildLetterObject.test.js
@@ -1,5 +1,7 @@
 const _ = require("lodash");
 const { buildLetterObject, buildCoordinatePair } = require("./buildLetterObject");
+const { importFromTextFileToArray } = require("../import-text-file/importTextFile");
+const { separateElementsFromRawPuzzleArray, elementType } = require("../separate-elements/separateElements");
 const { getTestLetterArray } = require("../test-data/testLetterArray");
 
 test("should return object with letter, x-coordinate, and y-coordinate given index and array", () => {
@@ -13,6 +15,22 @@ test("should return object with letter, x-coordinate, and y-coordinate given ind
 
 test("should return object of coordinate pairs by letter given letter array", () => {
   const letterObject = buildLetterObject(getTestLetterArray());
+
+  expect(Object.values(letterObject).length).toBe(26);
+  expect(letterObject["J"].length).toBe(9);
+  expect(letterObject["J"][2]).toEqual({ x: 17, y: 8, tried: false });
+  expect(letterObject["C"].length).toBe(22);
+  expect(letterObject["C"][14]).toEqual({ x: 19, y: 11, tried: false });
+});
+
+test("should return letterObject given text file is in puzzle-text-files directory", () => {
+  const puzzleTextFile = "samplePuzzle.txt";
+
+  const rawArray = importFromTextFileToArray(puzzleTextFile);
+
+  const letterArray = separateElementsFromRawPuzzleArray(elementType.LETTERS, rawArray);
+
+  const letterObject = buildLetterObject(letterArray);
 
   expect(Object.values(letterObject).length).toBe(26);
   expect(letterObject["J"].length).toBe(9);

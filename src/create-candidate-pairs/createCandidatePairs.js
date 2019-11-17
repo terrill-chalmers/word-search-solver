259394ae5a06377a332bd0b448c5d601a4c550b4
terrill-chalmers <terrill-chalmers@protonmail.com>
2019-11-16 18:08:33 -0600

    refactor: added ability for createCandidatePairs to return eight candidate pairs given a single pair starting location and return a single candidate pair given two or more pairs have established a direction

--- src/create-candidate-pairs/createCandidatePairs.js
+++ src/create-candidate-pairs/createCandidatePairs.js
@@ -1,14 +1,24 @@
-const createCandidatePairs = initialPair => {
-  return [
-    { x: initialPair.x - 1, y: initialPair.y - 1 },
-    { x: initialPair.x - 1, y: initialPair.y },
-    { x: initialPair.x, y: initialPair.y - 1 },
-    { x: initialPair.x - 1, y: initialPair.y + 1 },
-    { x: initialPair.x, y: initialPair.y + 1 },
-    { x: initialPair.x + 1, y: initialPair.y - 1 },
-    { x: initialPair.x + 1, y: initialPair.y },
-    { x: initialPair.x + 1, y: initialPair.y + 1 },
-  ];
+const { calculateDirection } = require("../search-direction/searchDirection");
+
+const createCandidatePairs = searchPath => {
+  const lastPair = searchPath[searchPath.length - 1];
+
+  if (searchPath.length === 1) {
+    return [
+      { x: lastPair.x - 1, y: lastPair.y - 1 },
+      { x: lastPair.x - 1, y: lastPair.y },
+      { x: lastPair.x, y: lastPair.y - 1 },
+      { x: lastPair.x - 1, y: lastPair.y + 1 },
+      { x: lastPair.x, y: lastPair.y + 1 },
+      { x: lastPair.x + 1, y: lastPair.y - 1 },
+      { x: lastPair.x + 1, y: lastPair.y },
+      { x: lastPair.x + 1, y: lastPair.y + 1 },
+    ];
+  } else {
+    const direction = calculateDirection(searchPath[0], searchPath[1]);
+
+    return [{ x: lastPair.x + direction.xOffset, y: lastPair.y + direction.yOffset }];
+  }
 };
 
 module.exports = { createCandidatePairs };
